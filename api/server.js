785d80fedfc2c656b411cae365c9e9c1d5048ea5
Nob Yuasys <yuasys@gmail.com>
2023-08-26 09:44:25 +0900

    modified:   api/server.js

--- api/server.js
+++ api/server.js
@@ -3,4 +3,10 @@ const app = express();
 
 const PORT = 5050; //注意　Macの場合5000だとコンフリクト（衝突）発生
 
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello</h1>")
+// });
+
+// console.log(typeof(app));
+
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
